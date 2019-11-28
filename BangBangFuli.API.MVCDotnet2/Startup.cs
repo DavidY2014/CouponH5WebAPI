a7687beee9ce1bf6f0988b5a5e85035a7f723558
袁大伟 <yuandawei@colipu.com>
2019-11-28 10:24:45 +0800

    fix

---
+++ BangBangFuli.API.MVCDotnet2/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangBangFuli.API.MVCDotnet2.Extensions;
+using BangBangFuli.H5.API.EntityFrameworkCore;
+using BangBangFuli.Utils.ORM.Imp;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BangBangFuli.API.MVCDotnet2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IDbContextManager<CouponSystemDBContext>>(s =>
+            {
+                return new DbContextManager<CouponSystemDBContext>(new ConnectionOption()
+                {
+                    Master = Configuration.GetConnectionString("H5BasicData"),
+                    SqlProvider = SqlProvider.SqlServer
+                });
+            });
+
+            //services.AddControllersWithViews();
+            services.AddByAssembly("BangBangFuli.H5.API.EntityFrameworkCore", "IBaseRepository");
+            services.AddByAssembly("BangBangFuli.H5.API.Application", "IAppService");
+            services.AddMvc();
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
