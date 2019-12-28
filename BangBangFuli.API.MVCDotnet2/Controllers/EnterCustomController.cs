29b3115037773c4b499c4f106c459f562fb498af
david <wu.xiguanghua@163.com>
2019-12-28 14:57:33 +0800

    fix

--- BangBangFuli.API.MVCDotnet2/Controllers/EnterCustomController.cs
+++ BangBangFuli.API.MVCDotnet2/Controllers/EnterCustomController.cs
@@ -19,9 +19,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace BangBangFuli.API.MVCDotnet2.Controllers
 {
     [UserLoginFilter]
-    public class EnterCustomController : Controller
+    public class EnterCustomController : BaseController
     {
-        private UserInfo _userinfo;
         private IUserRoleJurisdictionService _userRoleJurisdictionService;
         private IModuleInfoService _moduleInfoService;
         private readonly IBatchInformationService _batchInformationService;
@@ -37,7 +36,7 @@ namespace BangBangFuli.API.MVCDotnet2.Controllers
         public EnterCustomController(IUserRoleJurisdictionService userRoleJurisdictionService, IModuleInfoService moduleInfoService , 
             IProductInformationService productInformationService, IBatchInformationService batchInformationService, IHostingEnvironment hostingEnvironment,
             IBannerService bannerService , IBannerDetailService bannerDetailService,ICouponService couponService,
-            IOrderService orderService,IOrderDetailService orderDetailService)
+            IOrderService orderService,IOrderDetailService orderDetailService):base(userRoleJurisdictionService, moduleInfoService)
         {
             _hostingEnvironment = hostingEnvironment;
             _userRoleJurisdictionService = userRoleJurisdictionService;
@@ -50,20 +49,6 @@ namespace BangBangFuli.API.MVCDotnet2.Controllers
             _orderService = orderService;
             _orderDetailService = orderDetailService;
         }
-
-        /// <summary>
-        /// 当前登录的用户
-        /// </summary>
-        public new UserInfo User
-        {
-            get
-            {
-                if (_userinfo == null)
-                    _userinfo = JsonSerializerHelper.Deserialize<UserInfo>(HttpContext.Session.GetString("user"));
-                return _userinfo;
-            }
-        }
-
 
         /// <summary>
         /// 首页，工作台
@@ -661,36 +646,6 @@ namespace BangBangFuli.API.MVCDotnet2.Controllers
 
 
         #endregion
-
-
-
-        #region 基类
-
-        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
-        {
-            return base.OnActionExecutionAsync(context, next);
-        }
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            if (!context.HttpContext.Session.Keys.Contains("user"))
-            {
-                context.Result = new RedirectResult("/Account/Index");
-                return;
-            }
-            base.OnActionExecuting(context);
-        }
-        public override void OnActionExecuted(ActionExecutedContext context)
-        {
-            ViewData["user"] = User;
-            List<UserRoleJurisdiction> userrolejurlist = _userRoleJurisdictionService.GetList(User.RoleID);
-            ViewData["userrolejurlist"] = userrolejurlist;
-            List<ModuleInfo> modulelist = _moduleInfoService.GetList();
-            ViewData["modulelist"] = modulelist;
-            base.OnActionExecuted(context);
-        }
-
-        #endregion
-
 
         #region 分类枚举
         private void PopulateClassDropDownList(object selectedClass = null)
--- BangBangFuli.API.MVCDotnet2/Controllers/UsersController.cs
+++ BangBangFuli.API.MVCDotnet2/Controllers/UsersController.cs
@@ -13,7 +13,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace BangBangFuli.API.MVCDotnet2.Controllers
 {
-    public class UsersController : Controller
+    public class UsersController : BaseController
     {
         private IUserRoleJurisdictionService _userRoleJurisdictionService;
         private IModuleInfoService _moduleInfoService;
@@ -21,7 +21,7 @@ namespace BangBangFuli.API.MVCDotnet2.Controllers
         private readonly IUserService _userService;
 
         public UsersController(IUserRoleJurisdictionService userRoleJurisdictionService, IModuleInfoService moduleInfoService,IUserRoleService userRoleService,
-            IUserService userService)
+            IUserService userService):base(userRoleJurisdictionService,moduleInfoService)
         {
             _userRoleJurisdictionService = userRoleJurisdictionService;
             _moduleInfoService = moduleInfoService;
@@ -251,47 +251,5 @@ namespace BangBangFuli.API.MVCDotnet2.Controllers
         #endregion
 
 
-
-
-        #region 基类
-
-        private UserInfo _userinfo;
-        /// <summary>
-        /// 当前登录的用户
-        /// </summary>
-        public new UserInfo User
-        {
-            get
-            {
-                if (_userinfo == null)
-                    _userinfo = JsonSerializerHelper.Deserialize<UserInfo>(HttpContext.Session.GetString("user"));
-                return _userinfo;
-            }
-        }
-
-        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
-        {
-            return base.OnActionExecutionAsync(context, next);
-        }
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            if (!context.HttpContext.Session.Keys.Contains("user"))
-            {
-                context.Result = new RedirectResult("/Account/Index");
-                return;
-            }
-            base.OnActionExecuting(context);
-        }
-        public override void OnActionExecuted(ActionExecutedContext context)
-        {
-            ViewData["user"] = User;
-            List<UserRoleJurisdiction> userrolejurlist = _userRoleJurisdictionService.GetList(User.RoleID);
-            ViewData["userrolejurlist"] = userrolejurlist;
-            List<ModuleInfo> modulelist = _moduleInfoService.GetList();
-            ViewData["modulelist"] = modulelist;
-            base.OnActionExecuted(context);
-        }
-
-        #endregion
     }
 }
